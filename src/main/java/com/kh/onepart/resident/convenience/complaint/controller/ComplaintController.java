44093552847603a2e282b211712e6f9181a282cc
jkssh <jkssh@LAPTOP-76UB79P5>
2019-09-02 15:05:05 +0900

    2019_09_02_JJY

--- src/main/java/com/kh/onepart/resident/convenience/complaint/controller/ComplaintController.java
+++ src/main/java/com/kh/onepart/resident/convenience/complaint/controller/ComplaintController.java
@@ -1,5 +1,36 @@
 package com.kh.onepart.resident.convenience.complaint.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ComplaintController {
+	/*$("#menuComplaint").data("menu-url", "/onepart/resident/menuComplaint");*/
+	@RequestMapping("/resident/menuComplaint")
+	public String moveComplaint() {
+		System.out.println("/menuComplaint");
+		return "/resident/convenience/complaint/complaint";
+	}
+	
+	@RequestMapping("/resident/moveComplaintDetail")
+	public String moveComplaintDetail() {
+		System.out.println("/moveComplaintDetail");
+		return "/resident/convenience/complaint/complaint_detail";
+	}
+	
+	@RequestMapping("/resident/moveComplaintWrite")
+	public String moveComplaintWrite() {
+		System.out.println("/moveComplaintWrite");
+		return "/resident/convenience/complaint/complaint_write";
+	}
+	
+	@RequestMapping("/resident/moveComplaintUpdate")
+	public String moveComplaintUpdate() {
+		System.out.println("/moveComplaintUpdate");
+		return "/resident/convenience/complaint/complaint_update";
+	}
+	
+	
+
 
 }
--- src/main/java/com/kh/onepart/resident/my_apartment/notice/controller/NoticeController.java
+++ src/main/java/com/kh/onepart/resident/my_apartment/notice/controller/NoticeController.java
@@ -6,16 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class NoticeController {
 
+
+
+
 	@RequestMapping("/resident/menuNotice")
 	public String moveNotice() {
 		System.out.println("/menuNotice");
 		return "/resident/my_apartment/notice/notice";
 	}
 
-	@RequestMapping("/resident/moveNoticeTwo")
+	@RequestMapping("/resident/moveNoticeWrite")
 	public String moveNoticeTwo() {
-		System.out.println("/moveNoticeTwo");
-		return "/resident/my_apartment/notice/notice_two";
+		System.out.println("/moveNoticeWrite");
+		return "/resident/my_apartment/notice/notice_write";
 	}
-	
+	@RequestMapping("/resident/moveNoticeUpdate")
+	public String moveNoticeUpdate() {
+		System.out.println();
+		return "resident/my_apartment/notice/notice_update";
+	}
+
 }
