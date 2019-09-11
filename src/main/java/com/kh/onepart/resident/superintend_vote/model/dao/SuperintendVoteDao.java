ebf14ea01c1d3a7b1de6d60582f844f44e71ed8a
Kimeunhye95 <49854766+Kimeunhye95@users.noreply.github.com>
2019-09-11 20:31:19 +0900

    선관위 선거 시작

---
+++ src/main/java/com/kh/onepart/resident/superintend_vote/model/dao/SuperintendVoteDao.java
@@ -0,0 +1,30 @@
+package com.kh.onepart.resident.superintend_vote.model.dao;
+
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.kh.onepart.resident.superintend_vote.model.vo.ElectionVote;
+import com.kh.onepart.resident.superintend_vote.model.vo.GeneralVote;
+import com.kh.onepart.resident.superintend_vote.model.vo.GeneralVoteBdNm;
+import com.kh.onepart.resident.superintend_vote.model.vo.GeneralVoteCandidate;
+
+public interface SuperintendVoteDao {
+	//투표테이블 insert 메소드 (return gnrVoteSeq)
+	int insertGeneralVote(SqlSessionTemplate sqlSession, GeneralVote gv);
+	//동 투표일때 통투표 테이블 insert 메소드
+	int insertGeneralVoteBdNm(SqlSessionTemplate sqlSession, GeneralVoteBdNm generalVoteBdNm);
+	//해당 투표 후보정보 후보테이블 insert 메소드
+	int insertGeneralVoteCandidate(SqlSessionTemplate sqlSession, GeneralVoteCandidate generalVoteCandidate);
+	//선거정보 insert하는 메소드
+	int insertElectionVote(SqlSessionTemplate sqlSession, ElectionVote ev);
+	//현재 진행중인 선거 리스트 불러오는 메소드
+	ArrayList selectAllIngVoteList(SqlSessionTemplate sqlSession);
+	//일주일 이내에 종료된 선거 리스트 불러오는 메소드
+	ArrayList selectAllEndVoteList(SqlSessionTemplate sqlSession);
+	//해당 선거 정보 담아오는 메소드
+	ElectionVote selectOneElectionVote(SqlSessionTemplate sqlSession, int voteSeq);
+	//해당 선거에 등록된 후보 담아오는 메소드
+	ArrayList selectAllElectionVoteCandidate(SqlSessionTemplate sqlSession, int voteSeq);
+
+}
